363824a466d1e29b751e7db7a0c6b4c28b295704
Jonathan Prusik <jprusik@classynemesis.com>
2023-11-15 12:19:28 -0500

    add client module for handling custom data attributes between route changes

--- client/docusaurus.config.ts
+++ client/docusaurus.config.ts
@@ -30,6 +30,8 @@ const config: Config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
+  clientModules: ["./src/modules/customDataAttributes.js"],
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
---
+++ client/src/modules/customDataAttributes.js
@@ -0,0 +1,48 @@
+// Workaround for current custom data-attribute persistence
+// https://github.com/facebook/docusaurus/issues/9554
+
+export default module = {
+  onRouteUpdate({ location, previousLocation }) {
+    if (location.pathname !== previousLocation?.pathname) {
+      // copied from `docusaurus-theme-classic/src/index.ts` and modified
+      try {
+        // custom attributes
+        let attributesToRemove = [
+          "data-bare-page",
+          "data-hide-header",
+          "data-hide-pagination",
+          "data-hide-sidebar",
+        ];
+        const entries = [
+          ...new URLSearchParams(window.location.search).entries(),
+        ];
+        const DataQueryStringPrefixKey = "docusaurus-data-";
+
+        if (entries.length) {
+          for (var [searchKey, value] of entries) {
+            if (searchKey.startsWith(`${DataQueryStringPrefixKey}`)) {
+              var key = searchKey.replace(
+                `${DataQueryStringPrefixKey}`,
+                "data-"
+              );
+              document.documentElement.setAttribute(key, value);
+
+              // Don't remove the attribute if it's part of a present query string
+              attributesToRemove = attributesToRemove.filter(
+                (attributeKey) => attributeKey !== key
+              );
+            }
+          }
+        }
+
+        if (attributesToRemove.length) {
+          for (var attributeKey of attributesToRemove) {
+            try {
+              document.documentElement.attributes.removeNamedItem(attributeKey);
+            } catch {}
+          }
+        }
+      } catch (e) {}
+    }
+  },
+};
