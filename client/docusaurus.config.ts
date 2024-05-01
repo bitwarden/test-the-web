1f9796c1441e69f71b2113d3eaacaa3024be11f7
Jonathan Prusik <jprusik@classynemesis.com>
2024-05-01 16:55:47 -0400

    defer to env variable for site url value

--- client/docusaurus.config.ts
+++ client/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://localhost",
+  url: process.env.TEST_WEB_HOST || "https://localhost",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
