e36d5d948545a397ee9afcf7b7055f8e7ffea1e0
Jonathan Prusik <jprusik@users.noreply.github.com>
2024-06-24 09:15:43 -0400

    [PM-8873] Add pattern for deep iframe nesting for login items (#161)
    * allow Docusaurus to start in dev mode with SSL
    
    * add pattern for deep iframe nesting for login items
    
    * add nested iframe controls
    
    * add recursion bounds

---
+++ client/src/components/NestedIframeLogin.tsx
@@ -0,0 +1,122 @@
+import { useLocation } from "@docusaurus/router";
+import Admonition from "@theme/Admonition";
+import styled from "@emotion/styled";
+import {
+  EmailInput,
+  PasswordInput,
+  SubmitButton,
+  UsernameInput,
+} from "./Inputs";
+
+export function NestedIframeLogin({
+  slug = "nested-iframe-login",
+}: {
+  slug?: string;
+}): JSX.Element {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const depthParam = queryParams.get("depth") || "3";
+  const userInputType =
+    queryParams.get("userInputType") !== "email" ? "username" : "email";
+  const depth = parseInt(depthParam, 10);
+  const newDepth = depth - 1;
+
+  const iframeStyle = {
+    width: "100%",
+    minHeight: `calc(370px + ${depth}rem)`,
+  };
+
+  if (depth > 100) {
+    return (
+      <Admonition type="warning">
+        Depth value is too high; try a lower value.
+      </Admonition>
+    );
+  }
+
+  if (depth < 0) {
+    return (
+      <Admonition type="warning">
+        Depth value is too low; try a higher value.
+      </Admonition>
+    );
+  }
+
+  if (depth > 0) {
+    const src = `/forms/login/${slug}?depth=${newDepth}&docusaurus-data-bare-page=true`;
+
+    if (depth === 1) {
+      return (
+        <>
+          <FrameLabel>iframe depth: {depth}</FrameLabel>
+          <iframe
+            id="form-iframe-1"
+            title={`frame ${newDepth}`}
+            src={src + `&userInputType=username`}
+            style={{ ...iframeStyle, width: "40%", marginRight: "5%" }}
+            scrolling="no"
+            loading="lazy"
+          ></iframe>
+          <iframe
+            id="form-iframe-2"
+            title={`frame ${newDepth}`}
+            src={src + `&userInputType=email`}
+            style={{ ...iframeStyle, width: "40%" }}
+            scrolling="no"
+            loading="lazy"
+          ></iframe>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <FrameLabel>iframe depth: {depth}</FrameLabel>
+        <iframe
+          id="test-iframe"
+          title={`frame ${newDepth}`}
+          src={src}
+          style={iframeStyle}
+          loading="lazy"
+        ></iframe>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <FrameLabel>iframe depth: {depth}</FrameLabel>
+      <br />
+      <NestedForm userInputType={userInputType} />
+    </>
+  );
+}
+
+function NestedForm({
+  userInputType,
+}: {
+  userInputType: "username" | "email";
+}) {
+  return (
+    <>
+      <strong>Login with {userInputType}</strong>
+      <form className="card__body" method="POST" action="/login">
+        {userInputType === "username" ? <UsernameInput /> : <EmailInput />}
+        <PasswordInput />
+        <SubmitButton label="Submit" />
+      </form>
+    </>
+  );
+}
+
+const FrameLabel = styled.div`
+  border-bottom: 1px solid #3d3d3d;
+  line-height: 2rem;
+  font-weight: bold;
+
+  :before {
+    display: block;
+    margin-top: 10px;
+    content: "";
+  }
+`;
