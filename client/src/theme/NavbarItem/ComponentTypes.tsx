6525e013878eee4d73080c292b735b3ed4dcd80a
Jonathan Prusik <jprusik@users.noreply.github.com>
2024-06-24 09:59:57 -0400

    Fix react errors (#163)
    * fix invalid mobile attribute being passed to inline svg
    
    * fix className attribute on mdx docs

--- client/src/theme/NavbarItem/ComponentTypes.tsx
+++ client/src/theme/NavbarItem/ComponentTypes.tsx
@@ -4,12 +4,22 @@ import GithubLogo from "@site/static/img/icons/github.svg";
 import AngleRight from "@site/static/img/icons/angle-right.svg";
 import { Props as NavbarItemType } from "@theme/NavbarItem";
 
-function GithubIcon(props: NavbarItemType & InlineSVGProps): JSX.Element {
+function GithubIcon({
+  mobile, // do not pass to inline SVG
+  ...props
+}: NavbarItemType & InlineSVGProps): JSX.Element {
   return <InlineSVG {...{ ...props, Svg: GithubLogo }} />;
+}
+
+function AngleRightIcon({
+  mobile, // do not pass to inline SVG
+  ...props
+}: NavbarItemType & InlineSVGProps): JSX.Element {
+  return <InlineSVG {...{ ...props, Svg: AngleRight }} />;
 }
 
 export default {
   ...ComponentTypes,
   "custom-githubIcon": GithubIcon,
-  "custom-angleRight": AngleRight,
+  "custom-angleRight": AngleRightIcon,
 };
